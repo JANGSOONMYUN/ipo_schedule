2c54d8608226928740e034295ea9faa1f07f33d5
pc\user <jsm890803.3@gmail.com>
2024-06-21 16:20:03 +0900

    updating

--- crawl_ipo_list.py
+++ crawl_ipo_list.py
@@ -3,63 +3,66 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime, timedelta
 
-# 38커뮤니케이션 공모주 페이지 URL
-url = 'https://www.38.co.kr/html/fund/?o=k'
+def get_ipo_info():
+    # 38커뮤니케이션 공모주 페이지 URL
+    url = 'https://www.38.co.kr/html/fund/?o=k'
 
-# 페이지 요청
-response = requests.get(url)
-response.encoding = 'euc-kr'  # 인코딩 설정
+    # 페이지 요청
+    response = requests.get(url)
+    response.encoding = 'euc-kr'  # 인코딩 설정
 
-# BeautifulSoup 객체 생성
-soup = BeautifulSoup(response.text, 'html.parser', from_encoding='euc-kr')
+    # BeautifulSoup 객체 생성
+    soup = BeautifulSoup(response.text, 'html.parser', from_encoding='euc-kr')
 
-# 테이블 찾기
-table = soup.find('table', {'summary': '공모주 청약일정'})
+    # 테이블 찾기
+    table = soup.find('table', {'summary': '공모주 청약일정'})
 
-# 테이블이 제대로 선택되었는지 확인
-if table is None:
-    raise ValueError("Table with summary '공모주 청약일정' not found.")
+    # 테이블이 제대로 선택되었는지 확인
+    if table is None:
+        raise ValueError("Table with summary '공모주 청약일정' not found.")
 
-# 테이블 헤더 추출
-headers = [header.text.strip() for header in table.find_all('th')]
+    # 테이블 헤더 추출
+    headers = [header.text.strip() for header in table.find_all('th')]
 
-# '종목명' 헤더에 링크 추가를 위해 새로운 헤더 구성
-headers.insert(headers.index('종목명') + 1, '링크')
+    # '종목명' 헤더에 링크 추가를 위해 새로운 헤더 구성
+    headers.insert(headers.index('종목명') + 1, '링크')
 
-print(headers)
+    print(headers)
 
-# 데이터 추출
-rows = []
-for row in table.find_all('tr')[1:]:
-    do_pass = False
-    cols = row.find_all('td')
-    cols_data = []
-    for i, col in enumerate(cols):
-        # '종목명' 컬럼에서 링크 추출
-        if i == 0: 
-            link = ''
-            if col.find('a'):
-                link = col.find('a')['href']
-            if col.text is None or len(col.text) == 0 or '스팩' in col.text:
-                do_pass = True
-                break
-            cols_data.append(col.text.strip())
-            cols_data.append(link)
-        else:
-            cols_data.append(col.text.strip())
-    if not do_pass:
-        rows.append(cols_data)
+    # 데이터 추출
+    rows = []
+    for row in table.find_all('tr')[1:]:
+        do_pass = False
+        cols = row.find_all('td')
+        cols_data = []
+        for i, col in enumerate(cols):
+            # '종목명' 컬럼에서 링크 추출
+            if i == 0: 
+                link = ''
+                if col.find('a'):
+                    link = col.find('a')['href']
+                if col.text is None or len(col.text) == 0 or '스팩' in col.text:
+                    do_pass = True
+                    break
+                cols_data.append(col.text.strip())
+                cols_data.append(link)
+            else:
+                cols_data.append(col.text.strip())
+        if not do_pass:
+            rows.append(cols_data)
 
-# print(rows)
-# 데이터프레임 생성
-df = pd.DataFrame(rows, columns=headers)
+    # print(rows)
+    # 데이터프레임 생성
+    df = pd.DataFrame(rows, columns=headers)
+    return df
 
-# 오늘 날짜와 2주 전 날짜 계산
-today = datetime.today()
-two_weeks_ago = today - timedelta(weeks=2)
 
 # 날짜 범위를 처리하는 함수
 def is_within_two_weeks(date_range):
+    # 오늘 날짜와 2주 전 날짜 계산
+    today = datetime.today()
+    two_weeks_ago = today - timedelta(weeks=2)
+    
     start_date_str, end_date_str = date_range.split('~')
     start_date = datetime.strptime(start_date_str.strip(), '%Y.%m.%d')
     
@@ -72,8 +75,28 @@ def is_within_two_weeks(date_range):
     
     return (start_date >= two_weeks_ago) or (end_date >= two_weeks_ago)
 
+
+# 특정 페이지에서 상장일을 가져오는 함수
+def get_public_date(page_url):
+    response = requests.get(page_url)
+    response.encoding = 'euc-kr'
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # soup = BeautifulSoup(response.text, 'html.parser', from_encoding='euc-kr')
+    # 예시: 상장일 정보가 <td> 태그 내에 "상장일"이라는 텍스트가 있는 경우
+    public_date_td = soup.find('td', string='상장일')
+    if public_date_td:
+        return public_date_td.find_next_sibling('td').text.strip()
+    return None
+
+
+df = get_ipo_info()
 # '청약일'이 날짜 범위인 데이터를 필터링
 df_filtered = df[df['공모주일정'].apply(is_within_two_weeks)]
+
+# 상장일 받아오기
+public_date = get_public_date('https://www.38.co.kr/html/fund/?o=v&no=2053&l=&page=1')
+print(public_date)
+
 
 # 데이터프레임 출력
 print(df_filtered)
--- google_api.py
+++ google_api.py
@@ -1,6 +1,187 @@
+# import datetime
+# import pytz
+# import os.path
+# import json
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
+
+# # If modifying these SCOPES, delete the file token.json.
+# SCOPES = ['https://www.googleapis.com/auth/calendar']
+# # SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+
+# def load_config(file_path='config.json'):
+#     """Load configuration from a JSON file."""
+#     with open(file_path) as config_file:
+#         config = json.load(config_file)
+#     return config
+
+
+# def get_calendar_service():
+#     """Shows basic usage of the Google Calendar API.
+#     Prints the start and name of the next 10 events on the user's calendar.
+#     """
+#     creds = None
+#     # The file token.json stores the user's access and refresh tokens, and is
+#     # created automatically when the authorization flow completes for the first
+#     # time.
+#     if os.path.exists("token.json"):
+#         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+#     # If there are no (valid) credentials available, let the user log in.
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(
+#                 "credentials.json", SCOPES
+#             )
+#             creds = flow.run_local_server(port=0)
+#         # Save the credentials for the next run
+#         with open("token.json", "w") as token:
+#             token.write(creds.to_json())
+
+#     try:
+#         service = build("calendar", "v3", credentials=creds)
+#         return service
+#     except HttpError as error:
+#         print(f"An error occurred: {error}")
+#     return None
+
+# def test():
+#     try:
+#         service = get_calendar_service()
+
+#         # Call the Calendar API
+#         now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+#         print("Getting the upcoming 10 events")
+#         events_result = (
+#             service.events()
+#             .list(
+#                 calendarId="primary",
+#                 timeMin=now,
+#                 maxResults=10,
+#                 singleEvents=True,
+#                 orderBy="startTime",
+#             )
+#             .execute()
+#         )
+#         events = events_result.get("items", [])
+
+#         if not events:
+#             print("No upcoming events found.")
+#             return
+
+#         # Prints the start and name of the next 10 events
+#         for event in events:
+#             start = event["start"].get("dateTime", event["start"].get("date"))
+#             print(start, event["summary"])
+
+#     except HttpError as error:
+#         print(f"An error occurred: {error}")
+        
+# def get_upcoming_events(calendar_id, max_results=10):
+#     service = get_calendar_service()
+#     seoul_tz = pytz.timezone('Asia/Seoul')
+#     now = datetime.datetime.now(seoul_tz).isoformat()
+#     # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+#     events_result = service.events().list(calendarId=calendar_id).execute()
+#     events_result = service.events().list(calendarId=calendar_id, timeMin=now,
+#                                           maxResults=max_results, singleEvents=True,
+#                                           orderBy='startTime').execute()
+#     events = events_result.get('items', [])
+
+#     if not events:
+#         print('No upcoming events found.')
+#         return []
+
+#     # Prepare the data for JSON
+#     event_data = []
+#     for event in events:
+#         start = event['start'].get('dateTime', event['start'].get('date'))
+#         event_info = {
+#             'summary': event.get('summary', 'No title'),
+#             'start': start,
+#             'description': event.get('description', ''),
+#             'location': event.get('location', '')
+#         }
+#         event_data.append(event_info)
+
+#     # Write the data to a JSON file
+#     with open('upcoming_events.json', 'w') as json_file:
+#         json.dump(event_data, json_file, indent=4)
+
+#     print('Upcoming events data written to upcoming_events.json')
+#     return event_data
+
+# def get_specific_calendar(calendar_id):
+#     service = get_calendar_service()
+#     calendar = service.calendars().get(calendarId=calendar_id).execute()
+#     return calendar
+
+# def list_calendars():
+#     service = get_calendar_service()
+#     calendar_list = service.calendarList().list().execute()
+
+#     calendars = calendar_list.get('items', [])
+
+#     if not calendars:
+#         print('No calendars found.')
+#     for calendar in calendars:
+#         # print(calendar)
+#         print(f"Calendar Summary: {calendar['summary']}")
+#         print(f"Calendar ID: {calendar['id']}")
+#         print()
+#     return calendars
+
+# # example
+# def set_a_schedule(google_calendar, schedule_list: list):
+#     # google_calendar = GoogleCalendar()
+
+#     for schedule in schedule_list:
+#         date = datetime.date(year=2022, month=schedule["month"], day=schedule["day"]).isoformat()
+#         event = {
+#             "summary": schedule["team"],  # 일정 제목
+#             "location": schedule["stadium"],  # 일정 장소
+#             "description": '야구 경기',  # 일정 설명
+#             "start": {  # 시작 날짜
+#                 "dateTime": date + f"T{schedule['begin_time']}:00",
+#                 "timeZone": "Asia/Seoul",
+#             },
+#             "end": {  # 종료 날짜
+#                 "dateTime": date + f"T{schedule['end_time']}:59",
+#                 "timeZone": "Asia/Seoul",
+#             },
+#         }
+
+#         google_calendar.set_google_calendar(event)
+
+# if __name__ == "__main__":
+#     # test()
+    
+#     # get_specific_calendar()
+#     calendars = list_calendars()
+    
+#     ipo_calendar_id = ''
+#     for c in calendars:
+#         if '공모주' in c['summary']:
+#             ipo_calendar_id = c['id']
+#     # Write the data to a JSON file
+#     with open('ipo_calendar.json', 'w') as json_file:
+#         json.dump({'id': ipo_calendar_id, 'name': '공모주'}, json_file, indent=4)
+            
+#     ipo_calendar = get_specific_calendar(ipo_calendar_id)
+#     print('ipo_calendar_id:', ipo_calendar_id)
+#     print(ipo_calendar)
+    
+#     get_upcoming_events(ipo_calendar)
+    
 import datetime
 import os.path
 import json
+import pytz
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -8,61 +189,156 @@ from googleapiclient.discovery import build
 
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
+# SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-def load_config():
-    """Load configuration from a JSON file."""
-    with open('config.json') as config_file:
-        config = json.load(config_file)
-    return config
-
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Modifies a Google Calendar event.
-    """
-    config = load_config()
-
+def get_calendar_service():
     creds = None
+    
+    # When google.auth.exceptions.RefreshError: ('invalid_scope: Bad Request' ...) token should be regenerated. To make sure, comment 
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists(config['token_file']):
-        creds = Credentials.from_authorized_user_file(config['token_file'], SCOPES)
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                config['client_secret_file'], SCOPES)
+                'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open(config['token_file'], 'w') as token:
+        with open('token.json', 'w') as token:
             token.write(creds.to_json())
-
     service = build('calendar', 'v3', credentials=creds)
+    return service
 
-    # Call the Calendar API
-    calendar_id = config['calendar_id']
-    event_id = config['event_id']
-    
-    # Fetch the event
+def list_calendars():
+    service = get_calendar_service()
+    calendar_list = service.calendarList().list().execute()
+
+    calendars = calendar_list.get('items', [])
+
+    if not calendars:
+        print('No calendars found.')
+        return None
+
+    for calendar in calendars:
+        print(f"Calendar Summary: {calendar['summary']}")
+        print(f"Calendar ID: {calendar['id']}")
+        print()
+
+    return calendars
+
+def get_upcoming_events(calendar_id, max_results=20):
+    service = get_calendar_service()
+    seoul_tz = pytz.timezone('Asia/Seoul')
+    now = datetime.datetime.now(seoul_tz).isoformat()
+    events_result = service.events().list(calendarId=calendar_id, timeMin=now,
+                                          maxResults=max_results, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+        return []
+
+    # Prepare the data for JSON
+    event_data = []
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        event_info = {
+            'id': event['id'],
+            'summary': event.get('summary', 'No title'),
+            'start': start,
+            'description': event.get('description', ''),
+            'location': event.get('location', '')
+        }
+        event_data.append(event_info)
+
+    # # Write the data to a JSON file
+    # with open('upcoming_events.json', 'w') as json_file:
+    #     json.dump(event_data, json_file, ensure_ascii=False, indent=4)
+
+    # print('Upcoming events data written to upcoming_events.json')
+    return event_data
+
+def get_event_details(calendar_id, event_id):
+    service = get_calendar_service()
     event = service.events().get(calendarId=calendar_id, eventId=event_id).execute()
-    
-    # Modify the event
-    event['summary'] = 'Updated Event Summary'
-    event['description'] = 'Updated Event Description'
-    event['start'] = {
-        'dateTime': '2023-12-25T09:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    }
-    event['end'] = {
-        'dateTime': '2023-12-25T17:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    }
+    print(json.dumps(event, indent=4))
+    return event
 
-    updated_event = service.events().update(calendarId=calendar_id, eventId=event_id, body=event).execute()
+def add_event(calendar_id, event_details):
+    service = get_calendar_service()
+    event = service.events().insert(calendarId=calendar_id, body=event_details).execute()
+    print(f"Event created: {event.get('htmlLink')}")
+    return event
 
-    print('Event updated: %s' % (updated_event.get('htmlLink')))
+def update_event(calendar_id, event_id, updated_details):
+    service = get_calendar_service()
+    event = service.events().patch(calendarId=calendar_id, eventId=event_id, body=updated_details).execute()
+    print(f"Event updated: {event.get('htmlLink')}")
+    return event
+
+def delete_event(calendar_id, event_id):
+    service = get_calendar_service()
+    service.events().delete(calendarId=calendar_id, eventId=event_id).execute()
+    print(f"Event with ID {event_id} deleted.")
+    return
+
+def get_ipo_list():
+    # get ipo list
+    # 1. name
+    # 2. host_company
+    # 3. ipo_date
+    # 4. go_public_date
+    # 
+    ipo_list = []
+    return ipo_list
+
 
 if __name__ == '__main__':
-    main()
+    ipo_calendar_id = ''
+    if os.path.exists('ipo_calendar.json'):
+        with open('ipo_calendar.json', 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+            ipo_calendar_id = data['id']
+    else:
+        calendars = list_calendars()
+        for c in calendars:
+            if '공모주' in c['summary']:
+                ipo_calendar_id = c['id']
+                with open('ipo_calendar.json', 'w') as json_file:
+                    json.dump({'id': ipo_calendar_id, 'name': '공모주'}, json_file, indent=4)
+                # # Example: Selecting the first calendar ID from the list
+                # selected_calendar_id = c['id']
+                # print(selected_calendar_id)
+                # print(f"Selected Calendar ID: {selected_calendar_id}")
+                # get_upcoming_events(selected_calendar_id)
+                
+                break
+    # Example: Selecting the first calendar ID from the list
+    print(f"Selected Calendar ID: {ipo_calendar_id}")
+    events = get_upcoming_events(ipo_calendar_id)
+    # print(events)
+    
+    if events:
+        # Example: Get details of the first upcoming event
+        event_id = events[0]['id']
+        detail = get_event_details(ipo_calendar_id, event_id)
+        print(json.dumps(detail, ensure_ascii=False, indent=4))
+        
+        
+    ipo_list = get_ipo_list()
+        
+    for e in events:
+        event_id = e['id']
+        detail = get_event_details(ipo_calendar_id, event_id)
+        
+        for ipo in ipo_list:
+            if ipo['name'] in detail['summary']:
+                if ipo['ipo_date'] == detail['start']['dateTime']:
+                    pass 
